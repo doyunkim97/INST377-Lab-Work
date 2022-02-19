a2b65e7849cb80041fb7c3b1552ce26538e6fd9e
Do Yun Kim <94720911+doyunkim97@users.noreply.github.com>
2022-02-18 21:16:14 -0500

    js update

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -1,55 +1,89 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const grid = document.querySelector(".grid");
-  let squares = Array.from(document.querySelectorAll(".grid div"));
-  const ScoreDisplay = document.querySelector("#score");
-  const StartBtn = document.querySelector("#start-button");
-  const width = 10;
+document.addEventListener('DOMContentLoaded', () => {
+    const grid = document.querySelector('.grid')
+    let squares = Array.from(document.querySelectorAll('.grid div'))
+    const scoreDisplay = document.querySelector('#score')
+    const startBtn = document.querySelector('#start-button')
+    const width = 10
 
   // The Tetrominoes
-
   const lTetromino = [
-    [1, width + 1, width * 2 + 1, 2],
-    [width, width + 1, width + 2, width * 2 + 2],
-    [1, width + 1, width * 2 + 1, width * 2],
-    [width, width * 2, width * 2 + 1, width * 2 + 2],
-  ];
+    [1, width+1, width*2+1, 2],
+    [width, width+1, width+2, width*2+2],
+    [1, width+1, width*2+1, width*2],
+    [width, width*2, width*2+1, width*2+2]
+  ]
+
   const zTetromino = [
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-    [0, width, width + 1, width * 2 + 1],
-    [width + 1, width + 2, width * 2, width * 2 + 1],
-  ];
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1],
+    [0,width,width+1,width*2+1],
+    [width+1, width+2,width*2,width*2+1]
+  ]
 
   const tTetromino = [
-    [1, width, width + 1, width + 2],
-    [1, width + 1, width + 2, width * 2 + 1],
-    [width, width + 1, width + 2, width * 2 + 1],
-    [1, width, width + 1, width * 2 + 1],
-  ];
+    [1,width,width+1,width+2],
+    [1,width+1,width+2,width*2+1],
+    [width,width+1,width+2,width*2+1],
+    [1,width,width+1,width*2+1]
+  ]
 
   const oTetromino = [
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-    [0, 1, width, width + 1],
-  ];
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1],
+    [0,1,width,width+1]
+  ]
 
   const iTetromino = [
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-    [1, width + 1, width * 2 + 1, width * 3 + 1],
-    [width, width + 1, width + 2, width + 3],
-  ];
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3],
+    [1,width+1,width*2+1,width*3+1],
+    [width,width+1,width+2,width+3]
+  ]
+
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+  let currentPosition =4
+  let currentRotation =0
 
-let currentPostion = 4
-let current = theTetrominoes[0][0]
+  //randomly select a tetromino and its first rotation
+  let random = Math.floor(Math.random()*theTetrominoes.length)
+  let current = theTetrominoes[random][currentRotation];
 
-//first rotation
-function draw() {
+  //first rotation
+  function draw() {
     current.forEach(index => {
-        squares[currentPosition + index].classList.add('tetromino')
+      squares[currentPosition + index].classList.add('tetromino')
     })
+  }
+ // undrawing the tetromino
+ function undraw() {
+     current.forEach(index => {
+        squares[currentPosition+ index].classList.remove('tetromino')    
+     })
+ }
+
+ // tetromino move down every second
+
+ timerId= setInterval(moveDown, 1000)
+
+ // move down function
+ function moveDown() {
+     undraw()
+     currentPosition += width
+     draw()
+ }
+ 
+ //freeze function
+
+function freeze() {
+    if(current.some(index => squares[currentPosition + index + width].classList.contains('taken'))) {
+        current.forEach(index => squares[currentPosition + index].classList.add('taken'))
+        // new falling
+        random=math.floor(Math.random()*theTetrominoes.length)
+        current = theTetrominoes[random][currentRotation]
+        currentPosition=4
+        draw()
+    }
 }
 
 
@@ -61,5 +95,5 @@ function draw() {
 
 
 
-});
 
+})
