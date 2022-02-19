0695ff18733bedc59b6d895899f1fb25e6f9d517
Do Yun Kim <94720911+doyunkim97@users.noreply.github.com>
2022-02-18 20:15:23 -0500

    linking tetromino css from js

--- client/tetris/app.js
+++ client/tetris/app.js
@@ -41,4 +41,25 @@ document.addEventListener("DOMContentLoaded", () => {
     [width, width + 1, width + 2, width + 3],
   ];
   const theTetrominoes = [lTetromino, zTetromino, tTetromino, oTetromino, iTetromino]
+
+let currentPostion = 4
+let current = theTetrominoes[0][0]
+
+//first rotation
+function draw() {
+    current.forEach(index => {
+        squares[currentPosition + index].classList.add('tetromino')
+    })
+}
+
+
+
+
+
+
+
+
+
+
 });
+
