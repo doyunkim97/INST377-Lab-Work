e2ac43daab44009621ddf2ef16a22f159d5fa795
Do Yun Kim <94720911+doyunkim97@users.noreply.github.com>
2022-03-15 10:49:53 -0400

    added function getrandomintinclusive, datahandler

--- client/lab_6/script.js
+++ client/lab_6/script.js
@@ -1,18 +1,15 @@
+function getRandomIntInclusive(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-// As the last step of your lab, hook this up to index.html
-
-async function mainEvent() { // the async keyword means we can make API requests
-  const form = document.querySelector('.box');
-  form.addEventListener('submit', async (submitEvent) => { // async has to be declared all the way to get an await
-    submitEvent.preventDefault(); // This prevents your page from refreshing!
-    console.log('form submission'); // this is substituting for a "breakpoint"
-    const results = await fetch('/api/foodServicesPG'); // This accesses some data from our API
-    const arrayFromJson = await results.json(); // This changes it into data we can use - an object
-    console.table(arrayFromJson.data); // this is called "dot notation"
-    // arrayFromJson.data - we're accessing a key called 'data' on the returned object
-    // it contains all 1,000 records we need
+function dataHandler(dataArray) {
+  console.log('fired dataHandler');
+  console.table(dataArray);
+  const range = [...Array(15).keys()];
+  range.forEach((item) => {
+    console.log('range item', item);
   });
 }
 
-// this actually runs first! It's calling the function above
-document.addEventListener('DOMContentLoaded', async () => mainEvent()); // the async keyword means we can make API requests
